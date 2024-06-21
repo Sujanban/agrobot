8709dadb07805bd4a5938472dee37d3c33d4454a
Sujan Ban <bansujan@gmail.com>
2024-06-21 21:16:46 +0545

    chat route  implemented

--- server/controllers/chatController.js
+++ server/controllers/chatController.js
@@ -1,11 +1,31 @@
 const Chat = require("../models/chat.model");
+const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 const chat = async (req, res) => {
-    try{
-        res.json('Chat api')
-    }catch(err){
-        res.json(err.message)
-    }
+  try {
+    const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+    // getting user prompt
+    // const prompt = req.body.prompt;
+    const prompt = 'Hello';
+
+    // Save user prompt to database
+    const userMessage = new Chat({ user: "user", message: prompt });
+    await userMessage.save();
+
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const text = await response.text();
+
+    // Save bot response to database
+    const botMessage = new Chat({ user: "bot", message: text });
+    await botMessage.save();
+
+    res.json({ reply: text });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 module.exports = { chat };
