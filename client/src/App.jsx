d96050b7564d12c21f127ece27c5f342d269f194
Sujan Ban <bansujan@gmail.com>
2024-06-25 17:18:19 +0545

    shadcn added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,14 @@
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
+import { Button } from "@/components/ui/button"
+
 
 function App() {
   return (
     <>
-      <h1 className='text-3xl font-bold underline'>Hello</h1>
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+      </Routes>
     </>
   )
 }
