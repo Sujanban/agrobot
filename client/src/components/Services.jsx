8f5032d188eb92eec1db6f5932850a90db7e9415
Sujan Ban <bansujan@gmail.com>
2024-06-26 20:56:21 +0545

    heading added

--- client/src/components/Services.jsx
+++ client/src/components/Services.jsx
@@ -4,7 +4,7 @@ const Services = () => {
     const [activeTab, setActiveTab] = useState(1);
     return (
         <div className='px-4 py-20 max-w-7xl mx-auto '>
-            <h1 className='text-2xl max-w-xs'>Easy Farming: We Provides Best Services For Your Garden</h1>
+            <h1 className='text-2xl max-w-xs'><span className='text-[#878686]'>Smart Farming</span> : We Provides Best Services For Your Garden</h1>
             <div className='py-8 flex items-center justify-between'>
                 <div className='flex flex-col text-sm space-y-2'>
                     <li className='list-none'>
@@ -20,22 +20,31 @@ const Services = () => {
                 {
                     activeTab === 1 &&
                     <div className='flex space-x-4 max-w-3xl'>
-                        <img className='w-[200px] h-[150px] object-cover rounded-xl' src="https://plus.unsplash.com/premium_vector-1682307904375-1e8b895c745c?q=80&w=1800&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
-                        <p className='text-md'>Lorem ipsum nem quod. Nesciunt suscipit doloremque vitae fugit dignissimos voluptatem nobis adipisci mollitia neque eaque, hic natus eligendi quasi consequuntur labore sint asperiores est. Ipsa modi non sapiente consequuntur?</p>
+                        <img className='max-h-[150px] w-[200px] object-cover rounded-xl' src="https://plus.unsplash.com/premium_vector-1682307904375-1e8b895c745c?q=80&w=1800&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+                        <div>
+                            <h1 className='py-2 font-medium'>Farm Bot</h1>
+                            <p className='text-md'>Lorem ipsum nem quod. Nesciunt suscipit doloremque vitae fugit dignissimos voluptatem nobis adipisci mollitia neque eaque, hic natus eligendi quasi consequuntur labore sint asperiores est. Ipsa modi non sapiente consequuntur?</p>
+                        </div>
                     </div>
                 }
                 {
                     activeTab === 2 &&
                     <div className='flex space-x-4 max-w-3xl'>
-                        <img className='w-[200px] h-[150px] object-cover rounded-xl' src="https://plus.unsplash.com/premium_vector-1714142580885-97d3f0e39f98?q=80&w=1800&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
-                        <p className='text-md'>Lorem ipsum nem quod. Nesciunt suscipit doloremque vitae fugit dignissimos voluptatem nobis adipisci mollitia neque eaque, hic natus eligendi quasi consequuntur labore sint asperiores est. Ipsa modi non sapiente consequuntur?</p>
+                        <img className='max-h-[150px] w-[200px] object-cover rounded-xl' src="https://plus.unsplash.com/premium_vector-1714142580885-97d3f0e39f98?q=80&w=1800&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+                        <div>
+                            <h1 className='py-2 font-medium'>Weather Insight</h1>
+                            <p className='text-md'>Lorem ipsum nem quod. Nesciunt suscipit doloremque vitae fugit dignissimos voluptatem nobis adipisci mollitia neque eaque, hic natus eligendi quasi consequuntur labore sint asperiores est. Ipsa modi non sapiente consequuntur?</p>
+                        </div>
                     </div>
                 }
                 {
                     activeTab === 3 &&
                     <div className='flex space-x-4 max-w-3xl'>
-                        <img className='w-[200px] h-[150px] object-cover rounded-xl' src="https://plus.unsplash.com/premium_vector-1711987356317-e765e719f673?q=80&w=1800&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
-                        <p className='text-md'>Lorem ipsum nem quod. Nesciunt suscipit doloremque vitae fugit dignissimos voluptatem nobis adipisci mollitia neque eaque, hic natus eligendi quasi consequuntur labore sint asperiores est. Ipsa modi non sapiente consequuntur?</p>
+                        <img className='max-h-[150px] w-[200px] object-cover rounded-xl' src="https://plus.unsplash.com/premium_vector-1711987356317-e765e719f673?q=80&w=1800&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+                        <div>
+                            <h1 className='py-2 font-medium'>Disease Detection</h1>
+                            <p className='text-md'>Lorem ipsum nem quod. Nesciunt suscipit doloremque vitae fugit dignissimos voluptatem nobis adipisci mollitia neque eaque, hic natus eligendi quasi consequuntur labore sint asperiores est. Ipsa modi non sapiente consequuntur?</p>
+                        </div>
                     </div>
                 }
             </div>
