82815a6eaa01b118d06c49e037b02c4de07698e8
Sujan Ban <bansujan@gmail.com>
2024-06-27 21:08:51 +0545

    desktop navigation icon added

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -8,39 +8,41 @@ import { FaYoutube } from "react-icons/fa6";
 
 const Footer = () => {
   return (
-    <div className='px-4  max-w-7xl mx-auto '>
-      <div className='py-8 flex justify-between'>
-        <div className='space-y-4'>
-          <Link className='bg-green px-4 py-2 rounded-full text-white' to='/'>Agrobot</Link>
-          <p className='text-sm'>join us to improve your farming</p>
+    <section>
+      <div className='px-4  max-w-7xl mx-auto '>
+        <div className='py-8 flex justify-between'>
+          <div className='space-y-4'>
+            <Link className='bg-green px-4 py-2 rounded-full text-white' to='/'>Agrobot</Link>
+            <p className='text-sm'>join us to improve your farming</p>
+          </div>
+          <div className='space-y-2'>
+            <h1 className='font-medium text-lg'>Quick Navigation</h1>
+            <ul className='text-gray-500 grid grid-cols-2 gap-4'>
+              <li><Link to='/'>Home</Link></li>
+              <li><Link to='/services'>Services</Link></li>
+              <li><Link to='/howitworks'>How it works</Link></li>
+              <li><Link to='/contact'>Contact</Link></li>
+              <li><Link to='/about'>About</Link></li>
+            </ul>
+          </div>
+          <div className='flex space-x-2'>
+            <Link><FaFacebookF className="p-1 rounded-full bg-emerald-300" size={25} /></Link>
+            <Link><FaTwitter className="p-1 rounded-full bg-emerald-300" size={25} /></Link>
+            <Link><FaInstagram className="p-1 rounded-full bg-emerald-300" size={25} /></Link>
+            <Link><FaLinkedinIn className="p-1 rounded-full bg-emerald-300" size={25} /></Link>
+            <Link><FaYoutube className="p-1 rounded-full bg-emerald-300" size={25} /></Link>
+          </div>
         </div>
-        <div className='space-y-2'>
-          <h1 className='font-medium text-gray-500'>Quick Navigation</h1>
-          <ul className='grid grid-cols-2 gap-4'>
-            <li><Link to='/'>Home</Link></li>
-            <li><Link to='/services'>Services</Link></li>
-            <li><Link to='/howitworks'>How it works</Link></li>
-            <li><Link to='/contact'>Contact</Link></li>
-            <li><Link to='/about'>About</Link></li>
-          </ul>
-        </div>
-        <div className='flex space-x-2'>
-          <Link><FaFacebookF className="p-1 rounded-full bg-emerald-300" size={25}/></Link>
-          <Link><FaTwitter className="p-1 rounded-full bg-emerald-300" size={25}/></Link>
-          <Link><FaInstagram className="p-1 rounded-full bg-emerald-300" size={25}/></Link>
-          <Link><FaLinkedinIn className="p-1 rounded-full bg-emerald-300" size={25}/></Link>
-          <Link><FaYoutube className="p-1 rounded-full bg-emerald-300" size={25}/></Link>
+        <div className='text-sm border-t py-4 flex justify-between items-center'>
+          <h1>Copyright © 2022 Agrobot</h1>
+          <div className='flex items-center space-x-4'>
+            <Link>Terms & Conditions</Link>
+            <Link>Privacy Policy</Link>
+            <Link>Cookie Policy</Link>
+          </div>
         </div>
       </div>
-      <div className='text-sm border-t py-4 flex justify-between items-center'>
-        <h1>Copyright © 2022 Agrobot</h1>
-        <div className='flex items-center space-x-4'>
-          <Link>Terms & Conditions</Link>
-          <Link>Privacy Policy</Link>
-          <Link>Cookie Policy</Link>
-        </div>
-      </div>
-    </div>
+    </section>
   )
 }
 
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,10 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { HiMiniBars3BottomRight } from "react-icons/hi2";
+import { IoHomeOutline,IoChatboxOutline  } from "react-icons/io5";
+import { TbHotelService } from "react-icons/tb";
+import { FaQuestion } from "react-icons/fa6";
+import { SiAboutdotme } from "react-icons/si";
 
 
 const Navbar = () => {
@@ -9,40 +13,48 @@ const Navbar = () => {
     {
       id: 1,
       name: 'Home',
+      icon: <IoHomeOutline />,
       path: '/'
     },
     {
       id: 2,
       name: 'Services',
+      icon: <TbHotelService />,
       path: '/services'
     },
     {
       id: 3,
       name: 'How it Works',
+      icon: <FaQuestion />,
       path: '/howitworks'
     },
     {
       id: 4,
       name: 'Contact',
+      icon: <IoChatboxOutline />,
       path: '/contact'
     },
     {
       id: 5,
       name: 'About',
+      icon: <SiAboutdotme />,
       path: '/about'
     }
   ]
   return (
     <header className='shadow'>
       {/* desktop nav */}
-      <div className='hidden sm:flex h-[10vh] max-w-7xl mx-auto items-center justify-between'>
+      <div className=' hidden sm:flex h-[10vh] max-w-7xl mx-auto items-center justify-between'>
         <div>
           <Link className='text-xl font-bold'>Agrobot</Link>
         </div>
         <nav className='flex items-center space-x-4'>
           {
             menuItems.map(item => (
-              <Link className='p-2 hover:bg-slate-100 rounded-xl' key={item.id} to={item.path}>{item.name}</Link>
+              <Link className='shadow px-2 py-1 bg-white hover:bg-slate-100 rounded-xl flex items-center space-x-2' key={item.id} to={item.path}>
+                <span>{item.icon}</span> 
+                <span>{item.name}</span>
+              </Link>
             ))
           }
         </nav>
