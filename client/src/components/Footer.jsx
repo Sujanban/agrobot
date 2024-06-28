e333be1eb4caf1f7163ba9f43a0e49c51f08fc73
Sujan Ban <bansujan@gmail.com>
2024-06-28 20:44:44 +0545

    responsive footer implemented

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -10,10 +10,10 @@ const Footer = () => {
   return (
     <section>
       <div className='px-4  max-w-7xl mx-auto '>
-        <div className='py-8 flex justify-between'>
+        <div className='py-8 flex justify-between flex-wrap gap-4'>
           <div className='space-y-4'>
             <Link className='bg-green px-4 py-2 rounded-full text-white' to='/'>Agrobot</Link>
-            <p className='text-sm'>join us to improve your farming</p>
+            <p className='text-sm max-w-sm '>join us to improve the way you farm. </p>
           </div>
           <div className='space-y-2'>
             <h1 className='font-medium text-lg'>Quick Navigation</h1>
@@ -26,11 +26,11 @@ const Footer = () => {
             </ul>
           </div>
           <div className='flex space-x-2'>
-            <Link><FaFacebookF className="p-1 rounded-full bg-emerald-300" size={25} /></Link>
-            <Link><FaTwitter className="p-1 rounded-full bg-emerald-300" size={25} /></Link>
-            <Link><FaInstagram className="p-1 rounded-full bg-emerald-300" size={25} /></Link>
-            <Link><FaLinkedinIn className="p-1 rounded-full bg-emerald-300" size={25} /></Link>
-            <Link><FaYoutube className="p-1 rounded-full bg-emerald-300" size={25} /></Link>
+            <Link><FaFacebookF className="p-1 rounded-full shadow" size={25} /></Link>
+            <Link><FaTwitter className="p-1 rounded-full shadow" size={25} /></Link>
+            <Link><FaInstagram className="p-1 rounded-full shadow" size={25} /></Link>
+            <Link><FaLinkedinIn className="p-1 rounded-full shadow" size={25} /></Link>
+            <Link><FaYoutube className="p-1 rounded-full shadow" size={25} /></Link>
           </div>
         </div>
         <div className='text-sm border-t py-4 flex justify-between items-center'>
