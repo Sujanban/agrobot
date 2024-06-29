e9b8ef3d14144ffca33e95bbe114765ccc951c93
Sujan Ban <bansujan@gmail.com>
2024-06-29 09:10:29 +0545

    responsive howitworks section added

--- client/src/components/Howitworks.jsx
+++ client/src/components/Howitworks.jsx
@@ -9,21 +9,21 @@ const Howitworks = () => {
       <div className='px-4 py-8 max-w-7xl mx-auto '>
         <h1 className='font-medium text-4xl capitalize max-w-md'>Using our service is <span className='text-green'>simple</span>, <span className='text-green'>quick</span> and <span className='text-green'>easy</span>.</h1>
 
-        <div className='py-20 grid grid-cols-3 gap-4'>
+        <div className='py-20 grid md:grid-cols-3 gap-4'>
 
-          <div className='p-12 text-center max-w-md space-y-3 bg-white rounded-xl'>
+          <div className='p-8 sm:p-12 mx-auto text-center max-w-md space-y-3 bg-white rounded-xl'>
             <h1 className='text-xl font-medium capitalize'>Quick Accessing a farm bot model</h1>
             <img className=' mx-auto' src={bot} alt="" />
             <p className='text-gray-500 text-sm '>You you can easily accerss the farm bot by signuping into the platform using your email and password. You can find the chat bot section under the navigation bar on the left. </p>
           </div>
 
-          <div className='p-12 text-center max-w-md space-y-3 bg-white rounded-xl'>
+          <div className='p-8 sm:p-12 mx-auto text-center max-w-md space-y-3 bg-white rounded-xl'>
             <h1 className='text-xl font-medium capitalize'>Using a leaf disease detection model</h1>
             <img className='mx-auto' src={leaf} alt="" />
             <p className='text-gray-500 text-sm '>You you can easily accerss the farm bot by signuping into the platform using your email and password. You can find the chat bot section under the navigation bar on the left. </p>
           </div>
 
-          <div className='p-12 text-center max-w-md space-y-3 bg-white rounded-xl'>
+          <div className='p-8 sm:p-12 mx-auto text-center max-w-md space-y-3 bg-white rounded-xl'>
             <h1 className='text-xl font-medium capitalize'>Getting to the Weather insight page</h1>
             <img className='mx-auto ' src={weather} alt="" />
             <p className='text-gray-500 text-sm '>You you can easily accerss the farm bot by signuping into the platform using your email and password. You can find the chat bot section under the navigation bar on the left. </p>
