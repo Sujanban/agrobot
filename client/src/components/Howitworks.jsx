fa4a0bf2012216a90b119b5ce70675bee18da556
Sujan Ban <bansujan@gmail.com>
2024-06-29 21:05:27 +0545

    margin, padding fixed

--- client/src/components/Howitworks.jsx
+++ client/src/components/Howitworks.jsx
@@ -11,19 +11,19 @@ const Howitworks = () => {
 
         <div className='py-20 grid md:grid-cols-3 gap-4'>
 
-          <div className='p-8 sm:p-12 mx-auto text-center max-w-md space-y-3 bg-white rounded-xl'>
+          <div className='px-4 py-8 md:p-8 sm:p-12 mx-auto text-center max-w-md space-y-3 bg-white rounded-xl'>
             <h1 className='text-xl font-medium capitalize'>Quick Accessing a farm bot model</h1>
             <img className=' mx-auto' src={bot} alt="" />
             <p className='text-gray-500 text-sm '>You you can easily accerss the farm bot by signuping into the platform using your email and password. You can find the chat bot section under the navigation bar on the left. </p>
           </div>
 
-          <div className='p-8 sm:p-12 mx-auto text-center max-w-md space-y-3 bg-white rounded-xl'>
+          <div className='px-4 py-8 md:p-8 sm:p-12 mx-auto text-center max-w-md space-y-3 bg-white rounded-xl'>
             <h1 className='text-xl font-medium capitalize'>Using a leaf disease detection model</h1>
             <img className='mx-auto' src={leaf} alt="" />
             <p className='text-gray-500 text-sm '>You you can easily accerss the farm bot by signuping into the platform using your email and password. You can find the chat bot section under the navigation bar on the left. </p>
           </div>
 
-          <div className='p-8 sm:p-12 mx-auto text-center max-w-md space-y-3 bg-white rounded-xl'>
+          <div className='px-4 py-8 md:p-8 sm:p-12 mx-auto text-center max-w-md space-y-3 bg-white rounded-xl'>
             <h1 className='text-xl font-medium capitalize'>Getting to the Weather insight page</h1>
             <img className='mx-auto ' src={weather} alt="" />
             <p className='text-gray-500 text-sm '>You you can easily accerss the farm bot by signuping into the platform using your email and password. You can find the chat bot section under the navigation bar on the left. </p>
