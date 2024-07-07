ff610c70395792c0b110c657c580aaab18ffd960
Sujan Ban <bansujan@gmail.com>
2024-07-07 20:49:04 +0545

    login and register route added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,8 @@ import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import Index from './pages/Index'
 import Contact from './pages/Contact'
+import Register from './pages/Register'
+import Login from './pages/Login'
 
 
 function App() {
@@ -9,6 +11,8 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Index/>} />
+        <Route path='/register' element={<Register/>}/>
+        <Route path="/login" element={<Login/>} />
         <Route path="/contact" element={<Contact/>} />
       </Routes>
     </>
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div>
+      <div className='grid grid-cols-3 gap-4'>
+        <div>
+
+        </div>
+        <div className='p-4 bg-white rounded-xl shadow'>
+          <div>
+            <h1>Sign up as an organization</h1>
+            <p>Sign up with your work Google account or use the form.</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
