f2b35c2c80557b83b8d6989a256132c4c8d9250e
Sujan Ban <bansujan@gmail.com>
2024-07-16 18:29:10 +0545

    color code fixed

--- client/src/pages/dashboard/Weather.jsx
+++ client/src/pages/dashboard/Weather.jsx
@@ -17,7 +17,7 @@ const Weather = () => {
     return (
         <div className='flex'>
             <Navbar />
-            <div className='p-8 w-full space-y-4'>
+            <div className='h-screen overflow-auto p-8 w-full space-y-4'>
                 <div className='flex items-center justify-end space-x-2 text-custom-blue'>
                     <AiTwotoneQuestionCircle color='blue' />
                     <Link to={'/'} className='text-sm'>Help & Feedback</Link>
@@ -25,13 +25,13 @@ const Weather = () => {
                 <h1 className='font-semibold text-2xl'>Weather</h1>
 
                 <div className='grid grid-cols-3 gap-4'>
-                    <div className=' space-y-4 bg-black rounded-xl'>
+                    <div className=' space-y-4 bg-blasck rounded-xl border shadow'>
                         <div className='m-1 flex items-center gap-2 rounded-full bg-white'>
                             <IoLocationOutline className='text-5xl pl-4 py-2' />
                             <input className='p-3 w-full text-sm rounded-full outline-none' type="search" placeholder='Kathmandu, Nepal' />
                         </div>
 
-                        <div className='today-weather bg-black text-white p-4 rounded-xl space-y-4'>
+                        <div className='today-weather bg-blsack text-whiste p-4 rounded-xl space-y-4'>
                             <div className='py-8 text-center space-y-2'>
                                 <h1 className='text-5xl font-semibold'>28°C</h1>
                                 <h2 className='text-medium'>Rainy Day</h2>
@@ -82,12 +82,12 @@ const Weather = () => {
                     </div>
                     <div className='col-span-2 gap-4 w-full'>
                         <div className='grid gap-4'>
-                            <div className='p-4 rounded-xl bg-black'>
+                            <div className='p-4 rounded-xl bg-bddlack border shadow'>
                                 <div className='p-2 font-bold text-slate-500 flex items-center space-x-2 border-b-2 border-stone-600'>
                                     <LuClock3 /> <p>HOURLY FORECAST</p>
                                 </div>
-                                <div className='py-4 max-w-[46rem] overflow-auto flex items-center gap-4 text-white'>
-                                    <div className='px-5 py-3 bg-slate-900 grid gap-2 text-center rounded-md'>
+                                <div className='py-4 max-w-[46rem] overflow-auto flex items-center gap-4 text-whddite'>
+                                    <div className='px-5 py-3 bg-slate-200  grid gap-2 text-center rounded-md'>
                                         <h2 className='text-sm'>Now</h2>
                                         <h1 className='text-xl font-medium'>28°C</h1>
                                         <IoRainy className='text-2xl mx-auto' />
@@ -135,66 +135,66 @@ const Weather = () => {
                                 </div>
                             </div>
 
-                            <div className='p-4 rounded-xl bg-black'>
+                            <div className='p-4 rounded-xl bg-blacddk border shadow'>
                                 <div className='p-2 font-bold text-slate-500 flex items-center space-x-2 border-b-2 border-stone-600'>
                                     <CiCalendar /> <p>10-DAY FORECAST</p>
                                 </div>
-                                <div className='py-4 max-w-[46rem] overflow-auto flex items-center gap-4 text-white'>
-                                    <div className='px-5 py-3 bg-slate-900 grid gap-2 text-center rounded-md'>
+                                <div className='py-4 max-w-[46rem] overflow-auto flex items-center gap-4 text-whddite'>
+                                    <div className='px-6 py-3 bg-slate-200 grid gap-2 text-center rounded-md'>
                                         <h2 className='text-sm'>Today</h2>
                                         <p className='text-xs text-slate-500'>16/09</p>
                                         <h1 className='text-xl font-medium'>28°C</h1>
                                         <IoRainy className='text-2xl mx-auto' />
                                     </div>
-                                    <div className='px-5 py-3 bg-slate-900000 grid gap-2 text-center rounded-md'>
+                                    <div className='px-6 py-3 bg-slate-900000 grid gap-2 text-center rounded-md'>
                                         <h2 className='text-sm'>Thu</h2>
                                         <p className='text-xs text-slate-500'>17/09</p>
                                         <h1 className='text-xl font-medium'>28°C</h1>
                                         <IoRainy className='text-2xl mx-auto' />
                                     </div>
-                                    <div className='px-5 py-3 bg-slate-900000 grid gap-2 text-center rounded-md'>
+                                    <div className='px-6 py-3 bg-slate-900000 grid gap-2 text-center rounded-md'>
                                         <h2 className='text-sm'>Thu</h2>
                                         <p className='text-xs text-slate-500'>17/09</p>
                                         <h1 className='text-xl font-medium'>28°C</h1>
                                         <IoRainy className='text-2xl mx-auto' />
                                     </div>
-                                    <div className='px-5 py-3 bg-slate-900000 grid gap-2 text-center rounded-md'>
+                                    <div className='px-6 py-3 bg-slate-900000 grid gap-2 text-center rounded-md'>
                                         <h2 className='text-sm'>Thu</h2>
                                         <p className='text-xs text-slate-500'>17/09</p>
                                         <h1 className='text-xl font-medium'>28°C</h1>
                                         <IoRainy className='text-2xl mx-auto' />
                                     </div>
-                                    <div className='px-5 py-3 bg-slate-900000 grid gap-2 text-center rounded-md'>
+                                    <div className='px-6 py-3 bg-slate-900000 grid gap-2 text-center rounded-md'>
                                         <h2 className='text-sm'>Thu</h2>
                                         <p className='text-xs text-slate-500'>17/09</p>
                                         <h1 className='text-xl font-medium'>28°C</h1>
                                         <IoRainy className='text-2xl mx-auto' />
                                     </div>
-                                    <div className='px-5 py-3 bg-slate-900000 grid gap-2 text-center rounded-md'>
+                                    <div className='px-6 py-3 bg-slate-900000 grid gap-2 text-center rounded-md'>
                                         <h2 className='text-sm'>Thu</h2>
                                         <p className='text-xs text-slate-500'>17/09</p>
                                         <h1 className='text-xl font-medium'>28°C</h1>
                                         <IoRainy className='text-2xl mx-auto' />
                                     </div>
-                                    <div className='px-5 py-3 bg-slate-900000 grid gap-2 text-center rounded-md'>
+                                    <div className='px-6 py-3 bg-slate-900000 grid gap-2 text-center rounded-md'>
                                         <h2 className='text-sm'>Thu</h2>
                                         <p className='text-xs text-slate-500'>17/09</p>
                                         <h1 className='text-xl font-medium'>28°C</h1>
                                         <IoRainy className='text-2xl mx-auto' />
                                     </div>
-                                    <div className='px-5 py-3 bg-slate-900000 grid gap-2 text-center rounded-md'>
+                                    <div className='px-6 py-3 bg-slate-900000 grid gap-2 text-center rounded-md'>
                                         <h2 className='text-sm'>Thu</h2>
                                         <p className='text-xs text-slate-500'>17/09</p>
                                         <h1 className='text-xl font-medium'>28°C</h1>
                                         <IoRainy className='text-2xl mx-auto' />
                                     </div>
-                                    <div className='px-5 py-3 bg-slate-900000 grid gap-2 text-center rounded-md'>
+                                    <div className='px-6 py-3 bg-slate-900000 grid gap-2 text-center rounded-md'>
                                         <h2 className='text-sm'>Thu</h2>
                                         <p className='text-xs text-slate-500'>17/09</p>
                                         <h1 className='text-xl font-medium'>28°C</h1>
                                         <IoRainy className='text-2xl mx-auto' />
                                     </div>
-                                    <div className='px-5 py-3 bg-slate-900000 grid gap-2 text-center rounded-md'>
+                                    <div className='px-6 py-3 bg-slate-900000 grid gap-2 text-center rounded-md'>
                                         <h2 className='text-sm'>Thu</h2>
                                         <p className='text-xs text-slate-500'>17/09</p>
                                         <h1 className='text-xl font-medium'>28°C</h1>
@@ -204,7 +204,7 @@ const Weather = () => {
                             </div>
 
                             <div className='grid grid-cols-2 gap-4'>
-                                <div className='p-4 space-y-2 bg-black text-white rounded-xl '>
+                                <div className='p-4 space-y-2 bg-blfffack text-wfffhite rounded-xl border shadow'>
                                     <div className=' font-bold text-slate-500 flex items-center space-x-2'>
                                         <CiTempHigh className='' />
                                         <p className=''>UV INDEX</p>
@@ -214,12 +214,12 @@ const Weather = () => {
                                         <p className='font-medium'>Moderate</p>
                                     </div>
                                     <div className='relative w-full h-2 bg-red-500 rounded-full '>
-                                        <div className='absolute h-3 w-2 rounded bg-orange-600 border border-white  -top-0.5 left-[50%]'></div>
+                                        <div className='absolute h-3 w-2 rounded bg-orange-600 border border-wfffhite  -top-0.5 left-[50%]'></div>
                                     </div>
                                     <p className='text-xs text-stone-500'>use the sun protection until 16:00</p>
 
                                 </div>
-                                <div className='p-4 space-y-2 bg-black text-white rounded-xl'>
+                                <div className='p-4 space-y-2 bg-bffflack text-whdfdfite rounded-xl border shadow'>
                                     <div className=' font-bold text-slate-500 flex items-center space-x-2'>
                                         <MdOutlineWindPower className='' />
                                         <p className=''>WIND</p>
