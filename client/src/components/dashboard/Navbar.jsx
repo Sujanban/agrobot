480ce237d12c847ea915949d5ca06d4b34160b0e
Sujan Ban <bansujan@gmail.com>
2024-07-17 22:04:22 +0545

    icons changed with navbar footer section addition

--- client/src/components/dashboard/Navbar.jsx
+++ client/src/components/dashboard/Navbar.jsx
@@ -1,16 +1,14 @@
 import React, { useState } from 'react'
-import { PiBellBold } from "react-icons/pi";
 import { Link, NavLink } from 'react-router-dom';
-import { IoHomeOutline } from "react-icons/io5";
 import { TiWeatherShower } from "react-icons/ti";
 import { SiDependabot } from "react-icons/si";
 import { FaDisease } from "react-icons/fa";
-import { LuUsers2 } from "react-icons/lu";
-import { FaBarsStaggered } from "react-icons/fa6";
-import { IoIosArrowDropleft } from "react-icons/io";
-import { GoHome } from "react-icons/go";
 import { CiSearch } from "react-icons/ci";
 import { GiFireRing } from "react-icons/gi";
+import { IoSettingsOutline } from "react-icons/io5";
+import { HiOutlineSupport } from "react-icons/hi";
+import { FiUsers } from "react-icons/fi";
+import { RiHome6Line } from "react-icons/ri";
 
 
 const Navbar = () => {
@@ -20,7 +18,7 @@ const Navbar = () => {
         {
             name: 'Home',
             path: '/dashboard',
-            icon: <GoHome />
+            icon: <RiHome6Line />
         },
         {
             name: 'Weather',
@@ -40,7 +38,7 @@ const Navbar = () => {
         {
             name: 'Users',
             path: '/users',
-            icon: <LuUsers2 />
+            icon: <FiUsers />
         }
     ]
     return (
@@ -84,25 +82,50 @@ const Navbar = () => {
 
                 </div>
             </div>
-            <div className='grid gap-2'>
-                <div className='my-2 rounded-md shadow border flex items-center text-sm '>
-                    <CiSearch size={25} className='ml-2 text-gray-500' />
-                    <input className='p-2 w-full rounded-md outline-none' type="search" placeholder="Search..." />
-                </div>
-                {
-                    navigation.map(item => (
-                        <NavLink to={item.path} className={({ isActive }) =>
-                            isActive ?
-                                `p-2 text-sm font-medium rounded-md space-x-4 shadow border flex items-center`
-                                :
-                                `p-2 text-sm font-medium rounded-md space-x-4 hover:bg-stone-100 transition-all duration-200 ease-in-out flex items-center`
+            <div className='flex justify-between flex-col h-full'>
+                <div className='grid gap-2'>
+                    <div className='my-2 rounded-md shadow border flex items-center text-sm '>
+                        <CiSearch size={25} className='ml-2 text-gray-500' />
+                        <input className='p-2 w-full rounded-md outline-none' type="search" placeholder="Search..." />
+                    </div>
+                    {
+                        navigation.map(item => (
+                            <NavLink to={item.path} className={({ isActive }) =>
+                                isActive ?
+                                    `p-2 text-sm font-medium rounded-md space-x-4 shadow border flex items-center`
+                                    :
+                                    `p-2 text-sm font-medium rounded-md space-x-4 hover:bg-stone-100 transition-all duration-200 ease-in-out flex items-center`
 
-                        } key={item.name}>
-                            <span className='text-xl'>{item.icon}</span>
-                            {!minNavbar && <span>{item.name}</span>}
-                        </NavLink>
-                    ))
-                }
+                            } key={item.name}>
+                                <span className='text-xl'>{item.icon}</span>
+                                {!minNavbar && <span>{item.name}</span>}
+                            </NavLink>
+                        ))
+                    }
+                </div>
+                <div className='py-4 grid gap-2'>
+                    <NavLink to={'/support'} className={({ isActive }) =>
+                        isActive ?
+                            `p-2 text-sm font-medium rounded-md space-x-4 shadow border flex items-center`
+                            :
+                            `p-2 text-sm font-medium rounded-md space-x-4 hover:bg-stone-100 transition-all duration-200 ease-in-out flex items-center`
+
+                    }>
+                        <HiOutlineSupport className='text-xl' />
+                        {!minNavbar && <span>Support</span>}
+                    </NavLink>
+
+                    <NavLink to={'/settings'} className={({ isActive }) =>
+                        isActive ?
+                            `p-2 text-sm font-medium rounded-md space-x-4 shadow border flex items-center`
+                            :
+                            `p-2 text-sm font-medium rounded-md space-x-4 hover:bg-stone-100 transition-all duration-200 ease-in-out flex items-center`
+
+                    }>
+                        <IoSettingsOutline className='text-xl' />
+                        {!minNavbar && <span>Settings</span>}
+                    </NavLink>
+                </div>
             </div>
         </div>
     )
