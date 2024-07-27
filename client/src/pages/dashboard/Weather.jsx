031b373a7445b57faaaf171d64d33cb4aec2fe5e
Sujan Ban <bansujan@gmail.com>
2024-07-27 20:12:47 +0545

    city placeholder added

--- client/src/pages/dashboard/Weather.jsx
+++ client/src/pages/dashboard/Weather.jsx
@@ -6,8 +6,6 @@ import { IoEyeOutline } from "react-icons/io5";
 import { WiHumidity } from "react-icons/wi";
 import { LuClock3 } from "react-icons/lu";
 import { CiCalendar } from "react-icons/ci";
-import { MdOutlineWindPower } from "react-icons/md";
-import compass from "../../assets/compass.png"
 import { FaWind } from "react-icons/fa";
 
 
@@ -47,7 +45,7 @@ const Weather = () => {
                     <div className='flex-grow space-y-4 bg-blasck rounded-xl border shadow'>
                         <form onSubmit={handleSearch} className='m-1 shadow border flex items-center gap-2 rounded-full bg-white'>
                             <IoLocationOutline className='text-5xl pl-4 py-2' />
-                            <input onChange={(e) => setCity(e.target.value)} className='p-3 w-full text-sm rounded-full outline-none' type="search" placeholder='Kathmandu, Nepal' />
+                            <input onChange={(e) => setCity(e.target.value)} value={city} className='p-3 w-full text-sm rounded-full outline-none' type="search" placeholder='Kathmandu, Nepal' />
                         </form>
                         <div className='today-weather bg-blsack text-whiste p-4 rounded-xl space-y-4'>
                             <div className='py-8 text-center space-y-2'>
@@ -129,49 +127,6 @@ const Weather = () => {
                                     }
                                 </div>
                             </div>
-                            {/* <div className='grid grid-cols-2 gap-4'>
-                                <div className='p-4 space-y-2 bg-blfffack text-wfffhite rounded-xl border shadow'>
-                                    <div className=' font-bold text-slate-500 flex items-center space-x-2'>
-                                        <CiTempHigh className='' />
-                                        <p className=''>UV INDEX</p>
-                                    </div>
-                                    <div>
-                                        <h1 className='text-3xl'>3</h1>
-                                        <p className='font-medium'>Moderate</p>
-                                    </div>
-                                    <div className='relative w-full h-2 bg-red-500 rounded-full '>
-                                        <div className='absolute h-3 w-2 bg-orange-500  -top-0.5 left-[50%]'></div>
-                                    </div>
-                                    <p className='text-xs text-stone-500'>use the sun protection until 16:00</p>
-
-                                </div>
-                                <div className='p-4 space-y-2 bg-bffflack text-whdfdfite rounded-xl border shadow'>
-                                    <div className=' font-bold text-slate-500 flex items-center space-x-2'>
-                                        <MdOutlineWindPower className='' />
-                                        <p className=''>WIND</p>
-                                    </div>
-                                    <div className='grid grid-cols-2'>
-                                        <div className='space-y-2'>
-                                            <div className='py-2 flex flex-grow items-center space-x-2 border-b-2 border-slate-500'>
-                                                <h1 className='text-3xl'>3</h1>
-                                                <div className=''>
-                                                    <h1 className='font-bold text-sm text-slate-500'>MPH</h1>
-                                                    <p className='text-sm'>Wind</p>
-                                                </div>
-                                            </div>
-                                            <div className='flex flex-grow items-center space-x-2'>
-                                                <h1 className='text-3xl'>9</h1>
-                                                <div className=''>
-                                                    <h1 className='font-bold text-sm text-slate-500'>MPH</h1>
-                                                    <p className='text-sm'>Gusts</p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <img className='mx-auto h-20' src={compass} alt="" />
-                                    </div>
-
-                                </div>
-                            </div> */}
                         </div>
                     </div>
                 </div>
