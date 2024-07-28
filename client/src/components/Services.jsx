1ca1443e5ccdd816388d4cf71efe8bd92bc494fc
Sujan Ban <bansujan@gmail.com>
2024-07-28 20:39:19 +0545

    service description changed

--- client/src/components/Services.jsx
+++ client/src/components/Services.jsx
@@ -1,4 +1,8 @@
 import React, { useState } from 'react'
+import { FaArrowRightLong } from "react-icons/fa6";
+import { Link } from 'react-router-dom';
+import { MdArrowOutward } from "react-icons/md";
+
 
 const Services = () => {
     const [activeTab, setActiveTab] = useState(1);
@@ -20,34 +24,40 @@ const Services = () => {
                             <button onClick={() => setActiveTab(3)} className={`px-4 py-2 rounded-full ${activeTab === 3 ? 'bg-stone-9000 ' : 'text-gray-400'} `}>03. Disease Detection</button>
                         </li>
                     </div>
+                    <div>
+                        <FaArrowRightLong size={40} />
+                    </div>
                     {
                         activeTab === 1 &&
-                        <div className='relative flex  gap-4 max-w-3xl flex-wrap'>
-                            <img className='w-full object-cover rounded-xl' src="https://plus.unsplash.com/premium_vector-1682307904375-1e8b895c745c?q=80&w=1800&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+                        <div className='relative shadow flex  gap-4 max-w-3xl flex-wrap'>
+                            <img className='w-[700px] h-[500px] object-cover rounded-xl' src="https://plus.unsplash.com/premium_vector-1682307904375-1e8b895c745c?q=80&w=1800&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
                             {/* <div className='absolute bottom-0 left-0'>
                                 <h1 className='py-2 font-medium'>Farm Bot</h1>
                                 <p className='text-md'>Lorem ipsum nem quod. Nesciunt suscipit doloremque vitae fugit dignissimos voluptatem nobis adipisci mollitia neque eaque, hic natus eligendi quasi consequuntur labore sint asperiores est. Ipsa modi non sapiente consequuntur?</p>
                             </div> */}
+                            <Link className='m-2 px-4 py-2 rounded-full bg-slate-200 hover:bg-slate-300 text-sm flex items-center space-x-2 absolute bottom-0 left-0' to='/'><span>Try now</span> <MdArrowOutward /></Link>
                         </div>
                     }
                     {
                         activeTab === 2 &&
-                        <div className='flex  gap-4 max-w-3xl flex-wrap'>
-                            <img className='h-[250px] w-[400px] object-cover rounded-xl' src="https://plus.unsplash.com/premium_vector-1714142580885-97d3f0e39f98?q=80&w=1800&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
-                            <div className=''>
+                        <div className='relative shadow'>
+                            <img className='w-[700px] h-[500px] object-cover rounded-xl' src="https://plus.unsplash.com/premium_vector-1714142580885-97d3f0e39f98?q=80&w=1800&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+                            {/* <div className=''>
                                 <h1 className='py-2 font-medium'>Weather Insight</h1>
                                 <p className='text-md'>Lorem ipsum nem quod. Nesciunt suscipit doloremque vitae fugit dignissimos voluptatem nobis adipisci mollitia neque eaque, hic natus eligendi quasi consequuntur labore sint asperiores est. Ipsa modi non sapiente consequuntur?</p>
-                            </div>
+                            </div> */}
+                            <Link className='m-2 px-4 py-2 rounded-full bg-slate-200 hover:bg-slate-300 text-sm flex items-center space-x-2 absolute bottom-0 left-0' to='/'><span>Try now</span> <MdArrowOutward /></Link>
                         </div>
                     }
                     {
                         activeTab === 3 &&
-                        <div className='flex gap-4 max-w-3xl flex-wrap'>
-                            <img className='h-[250px] w-[400px] object-cover rounded-xl' src="https://plus.unsplash.com/premium_vector-1711987356317-e765e719f673?q=80&w=1800&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
-                            <div className=''>
+                        <div className='relative shadow'>
+                            <img className=' w-[700px] h-[500px] object-cover rounded-xl' src="https://plus.unsplash.com/premium_vector-1711987356317-e765e719f673?q=80&w=1800&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+                            {/* <div className=''>
                                 <h1 className='py-2 font-medium'>Disease Detection</h1>
                                 <p className='text-md'>Lorem ipsum nem quod. Nesciunt suscipit doloremque vitae fugit dignissimos voluptatem nobis adipisci mollitia neque eaque, hic natus eligendi quasi consequuntur labore sint asperiores est. Ipsa modi non sapiente consequuntur?</p>
-                            </div>
+                            </div> */}
+                            <Link className='m-2 px-4 py-2 rounded-full bg-slate-200 hover:bg-slate-300 text-sm flex items-center space-x-2 absolute bottom-0 left-0' to='/'><span>Try now</span> <MdArrowOutward /></Link>
                         </div>
                     }
                 </div>
