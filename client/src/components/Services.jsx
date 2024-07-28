4169f471187c944f45a98462fcf5deccb7cc7aaa
Sujan Ban <bansujan@gmail.com>
2024-07-28 20:26:17 +0545

    service pagination btn asthetics changed

--- client/src/components/Services.jsx
+++ client/src/components/Services.jsx
@@ -10,24 +10,24 @@ const Services = () => {
                     : We Provides Best Services For Your Garden</h1>
                 <div className='py-8 pb-20 flex items-center justify-between gap-4 flex-wrap'>
                     <div className='flex flex-col text-sm space-y-2'>
-                        <li className='list-none'>
-                            <button onClick={() => setActiveTab(1)} className={`px-4 py-2 rounded-full ${activeTab === 1 ? 'bg-green text-white' : 'bg-greenn'} `}>01. Farm Bot</button>
+                        <li className='list-none text-3xl'>
+                            <button onClick={() => setActiveTab(1)} className={`px-4 py-2 rounded-full ${activeTab === 1 ? 'bg-stone-9000 ' : 'text-gray-400'} `}>01. Farm Bot</button>
                         </li>
-                        <li className='list-none'>
-                            <button onClick={() => setActiveTab(2)} className={`px-4 py-2 rounded-full ${activeTab === 2 ? 'bg-green text-white' : 'bg-greenn'} `}>02. Weather Insight</button>
+                        <li className='list-none text-3xl'>
+                            <button onClick={() => setActiveTab(2)} className={`px-4 py-2 rounded-full ${activeTab === 2 ? 'bg-stone-9000 ' : 'text-gray-400'} `}>02. Weather Insight</button>
                         </li>
-                        <li className='list-none'>
-                            <button onClick={() => setActiveTab(3)} className={`px-4 py-2 rounded-full ${activeTab === 3 ? 'bg-green text-white' : 'bg-greenn'} `}>03. Disease Detection</button>
+                        <li className='list-none text-3xl'>
+                            <button onClick={() => setActiveTab(3)} className={`px-4 py-2 rounded-full ${activeTab === 3 ? 'bg-stone-9000 ' : 'text-gray-400'} `}>03. Disease Detection</button>
                         </li>
                     </div>
                     {
                         activeTab === 1 &&
-                        <div className='flex gap-4 max-w-lg flex-wrap'>
-                            <img className='h-[250px] w-[400px] object-cover rounded-xl' src="https://plus.unsplash.com/premium_vector-1682307904375-1e8b895c745c?q=80&w=1800&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
-                            <div className=''>
+                        <div className='relative flex  gap-4 max-w-3xl flex-wrap'>
+                            <img className='w-full object-cover rounded-xl' src="https://plus.unsplash.com/premium_vector-1682307904375-1e8b895c745c?q=80&w=1800&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+                            {/* <div className='absolute bottom-0 left-0'>
                                 <h1 className='py-2 font-medium'>Farm Bot</h1>
                                 <p className='text-md'>Lorem ipsum nem quod. Nesciunt suscipit doloremque vitae fugit dignissimos voluptatem nobis adipisci mollitia neque eaque, hic natus eligendi quasi consequuntur labore sint asperiores est. Ipsa modi non sapiente consequuntur?</p>
-                            </div>
+                            </div> */}
                         </div>
                     }
                     {
