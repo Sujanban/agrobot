54315489e3adf33cdc740aba6f3dc1432f394b5a
Sujan Ban <bansujan@gmail.com>
2024-08-01 20:27:50 +0545

    button optimized

--- client/src/components/Tesimonial.jsx
+++ client/src/components/Tesimonial.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import user1 from "../assets/user1.png"
 import user2 from "../assets/user2.png"
 import { FaQuoteLeft } from "react-icons/fa6";
+import { MdArrowOutward } from "react-icons/md";
 
 const Tesimonial = () => {
     return (
@@ -25,11 +25,11 @@ const Tesimonial = () => {
                                 <FaQuoteLeft className='inline-flex mr-2' size={15} />
                                 Client Testimonial
                             </button>
-                            <h1 className='text-3xl font-medium '>I was amaized by the level of assistance received from the AI Chatbot provided by Agrobot. It quickly undeerstood my needs and provided me with the best possible solutions.</h1>
+                            <h1 className='py-2 text-3xl font-medium  '>I was amaized by the level of assistance received from the AI Chatbot provided by Agrobot. It quickly undeerstood my needs and provided me with the best possible solutions.</h1>
                         </div>
                         <div className='p-4 border-t-2 border-slate-300 flex justify-between items-center'>
                             <button className='border rounded-full px-6 py-2 border-black'>1/4</button>
-                            <button className='font-thin border rounded-full px-8 py-2 text-white bg-black'>See more testimonials</button>
+                            <button className='text-white/80 border rounded-full px-8 py-2 text-white bg-black'>See more testimonials <MdArrowOutward className='inline-flex'/></button>
                         </div>
                     </div>
                 </div>
