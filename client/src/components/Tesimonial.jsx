44cca606dae942c95439b4c7b09aca8166a15bab
Sujan Ban <bansujan@gmail.com>
2024-08-01 20:29:50 +0545

    code cleanup

--- client/src/components/Tesimonial.jsx
+++ client/src/components/Tesimonial.jsx
@@ -6,7 +6,7 @@ import { MdArrowOutward } from "react-icons/md";
 const Tesimonial = () => {
     return (
         <section className='bg-slate-100'>
-            <div className='px-4 py-20 max-w-7xl mx-auto space-y-4 '>
+            <div className='px-4 py-20 max-w-7xl mx-auto'>
                 <div className='flex items-center space-x-4'>
                     <h1 className='p-8 text-5xl leading-normal font-medium bg-white rounded-xl'>What top-tier farmers tell about our products</h1>
                     <p className='max-w-sm'>Join the growing number of business and companies that have experianced that we have transformative impact on the smart AI integration service.</p>
@@ -19,7 +19,7 @@ const Tesimonial = () => {
                             <p>Founder GOTHALO INC.</p>
                         </div>
                     </div>
-                    <div className='space-y-4 flex flex-col'>
+                    <div className='p-2 rounded-3xl bg-white space-y-4 flex flex-col'>
                         <div className='p-4 testimonial-banner space-y-2 grow'>
                             <button className='font-medium text-sm border rounded-full px-4 py-2 border-black'>
                                 <FaQuoteLeft className='inline-flex mr-2' size={15} />
@@ -29,7 +29,7 @@ const Tesimonial = () => {
                         </div>
                         <div className='p-4 border-t-2 border-slate-300 flex justify-between items-center'>
                             <button className='border rounded-full px-6 py-2 border-black'>1/4</button>
-                            <button className='text-white/80 border rounded-full px-8 py-2 text-white bg-black'>See more testimonials <MdArrowOutward className='inline-flex'/></button>
+                            <button className='text-white/80 border rounded-full px-8 py-2 text-white bg-black'>See more testimonials <MdArrowOutward className='inline-flex' /></button>
                         </div>
                     </div>
                 </div>
