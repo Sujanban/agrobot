b05cfaea1432f076dfb5b4a194ddd89718a40750
Sujan Ban <bansujan@gmail.com>
2024-08-02 20:38:11 +0545

    footer updated

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -8,39 +8,48 @@ import { FaYoutube } from "react-icons/fa6";
 
 const Footer = () => {
   return (
-    <section className='relative'>
-      <div className='hidden md:block left-0 -top-[20%] absolute w-1/2 rounded-r-full h-32  p-4 bg-white'></div>
-      <div className='hidden md:block right-0 -top-[20%] absolute w-1/2 rounded-l-full h-32  p-4 bg-white'></div>
-      <div className='px-4  max-w-7xl mx-auto'>
-        <div className='py-20 flex justify-between flex-wrap gap-4'>
-          <div className='space-y-4'>
-            <Link className='bg-green px-4 py-2 rounded-full text-white' to='/'>Agrobot</Link>
-            <p className='text-sm max-w-sm '>Join us to improve the way you farm. </p>
-          </div>
-          <div className='space-y-2'>
-            <h1 className='font-medium text-lg'>Quick Navigation</h1>
-            <ul className='text-gray-500 grid grid-cols-2 gap-4'>
-              <li><Link to='/'>Home</Link></li>
-              <li><Link to='/services'>Services</Link></li>
-              <li><Link to='/howitworks'>How it works</Link></li>
-              <li><Link to='/contact'>Contact</Link></li>
-              <li><Link to='/about'>About</Link></li>
-            </ul>
-          </div>
-          <div className='flex space-x-2'>
-            <Link><FaFacebookF className="p-1 rounded-full shadow" size={25} /></Link>
-            <Link><FaTwitter className="p-1 rounded-full shadow" size={25} /></Link>
-            <Link><FaInstagram className="p-1 rounded-full shadow" size={25} /></Link>
-            <Link><FaLinkedinIn className="p-1 rounded-full shadow" size={25} /></Link>
-            <Link><FaYoutube className="p-1 rounded-full shadow" size={25} /></Link>
+    <section className=' text-white'>
+      <div className='bg-stone-900 rounded-xl max-w-7xl mx-auto'>
+        <div className=' px-12 py-20 '>
+          <div className='flex justify-between'>
+            <div className='flex space-x-16'>
+              <div className='grid space-y-4'>
+                <h1 className='text-xl'>Company</h1>
+                <Link className='text-white/70'>About Agrobot</Link>
+                <Link className='text-white/70'>Pricing</Link>
+                <Link className='text-white/70'>Demo</Link>
+              </div>
+
+              <div className='grid space-y-4'>
+                <h1 className='text-xl'>Resources</h1>
+                <Link className='text-white/70'>Blog</Link>
+                <Link className='text-white/70'>Services</Link>
+                <Link className='text-white/70'>Tool Library</Link>
+              </div>
+
+              <div className='grid space-y-4'>
+                <h1 className='text-xl'>Legal</h1>
+                <Link className='text-white/70'>Terms of Use</Link>
+                <Link className='text-white/70'>Privacy Policy</Link>
+              </div>
+            </div>
+            <div className='max-w-xl space-y-4'>
+              <Link className='text-4xl font-semibold'>Agrobot</Link>
+              <p className='text-white/70'>We are the forefront of Smart AI development, revolutingly the way farmers and business grow their crops more effectively.</p>
+            </div>
           </div>
         </div>
-        <div className='text-sm border-t py-4 flex justify-between items-center flex-wrap'>
-          <h1>Copyright © 2022 Agrobot</h1>
-          <div className='flex items-center space-x-4 flex-wrap'>
-            <Link>Terms & Conditions</Link>
-            <Link>Privacy Policy</Link>
-            <Link>Cookie Policy</Link>
+        <div className='px-12 '>
+          <div className='text-sm flex items-center py-8 border-t-2 border-stone-600 text-white/70 space-x-2'>
+            <div className='space-x-2'>
+              <Link className='px-4 py-2 border rounded-full'>Instagram</Link>
+              <Link className='px-4 py-2 border rounded-full'>Linkedin</Link>
+              <Link className='px-4 py-2 border rounded-full'>Twitter</Link>
+              <Link className='px-4 py-2 border rounded-full'>Github</Link>
+            </div>
+            <div className='grow border text-center rounded-full px-4 py-2 '>
+              <Link className=''>Agrobot. All Rights Reserved.</Link>
+            </div>
           </div>
         </div>
       </div>
