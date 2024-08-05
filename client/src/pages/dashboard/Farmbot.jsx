0703ee86f64dc1d52daff4dbecd758a1ee8308a8
Sujan Ban <bansujan@gmail.com>
2024-08-05 20:34:26 +0545

    chatbot feature implemented

--- client/src/pages/dashboard/Farmbot.jsx
+++ client/src/pages/dashboard/Farmbot.jsx
@@ -2,12 +2,15 @@ import Navbar from '@/components/dashboard/Navbar'
 import React, { useState, useEffect } from 'react'
 import { IoPaperPlaneOutline } from "react-icons/io5";
 import chatbot from '../../assets/chatbot.png'
-import axios from "axios";
+import ReactMarkdown from "react-markdown";
+import BeatLoader from "react-spinners/BeatLoader";
+
 
 const Farmbot = () => {
     const [question, setQuestion] = useState("");
     const [messages, setMessages] = useState([]);
     const [generatingAnswer, setGeneratingAnswer] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
         const savedMessages =
@@ -28,17 +31,30 @@ const Farmbot = () => {
         setQuestion("");
 
         try {
-            const response = await axios({
-                url: `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${process.env.NEXT_PUBLIC_API_GENERATIVE_LANGUAGE_CLIENT}`,
-                method: "post",
-                data: {
-                    contents: [{ parts: [{ text: newMessage.text }] }],
+            setIsLoading(true);
+            const response = await fetch('https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=' + import.meta.env.VITE_GEMINI_API_KEY + '', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
                 },
+                body: JSON.stringify({
+                    contents: [{ parts: [{ text: newMessage.text }] }]
+                })
             });
+
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+
+            const data = await response.json();
+
+            console.log(data);
+
+            setIsLoading(false);
 
             const botMessage = {
                 sender: "bot",
-                text: response.data.candidates[0].content.parts[0].text,
+                text: data.candidates[0].content.parts[0].text,
             };
             setMessages((prev) => [...prev, botMessage]);
         } catch (error) {
@@ -53,6 +69,7 @@ const Farmbot = () => {
         setGeneratingAnswer(false);
     }
 
+    console.log(messages)
 
     return (
         <div className='flex max-w-7xl mx-auto'>
@@ -62,48 +79,54 @@ const Farmbot = () => {
                 <div>
                     <div className='p-2 shadow border max-w-xl mx-auto overflow-auto  space-y-4 bordder shaddow rounded-xl'>
                         <p className='p-4 bg-slate-200 font-medium rounded-md text-sm underline'>Ask the chatbot with your queries related to farming and agriculture.</p>
-                        <div className='p-4 space-y-4 h-[60vh] overflow-auto'>
-                            <div className='py-2 flex space-x-4'>
-                                <h1 className='h-10 w-10 flex items-center justify-center shrink-0 text-white  rounded-full bg-stone-800'>S</h1>
-                                <div className='space-y-2'>
-                                    <h1 className='font-semibold'>You</h1>
-                                    <p className='p-2 bg-stone-900 text-white rounded-r-xl rounded-b-xl text-xs'>
-                                        Hi
-                                    </p>
-                                </div>
-                            </div>
-                            <div className='py-2 flex space-x-4 justify-end'>
-                                <div className='space-y-2'>
-                                    <h1 className='font-semibold text-right'>Agrobot</h1>
-                                    <p className='p-2 bg-gray-200 text-black rounded-l-xl rounded-b-xl text-xs'>
-                                        Hello! How can I assist you today?
-                                    </p>
-                                </div>
-                                <h1 className='h-10 w-10 flex items-center justify-center shrink-0 text-white  rounded-full bg-custom-green'>
-                                    <img src={chatbot} alt="" />
-                                </h1>
-                            </div>
-                            <div className='py-2 flex space-x-4'>
-                                <h1 className='h-10 w-10 flex items-center justify-center shrink-0 text-white  rounded-full bg-stone-800'>S</h1>
-                                <div className='space-y-2'>
-                                    <h1 className='font-semibold'>You</h1>
-                                    <p className='p-2 bg-stone-900 text-white rounded-r-xl rounded-b-xl text-xs'>
-                                        what is farming
-                                    </p>
-                                </div>
-                            </div>
-                            <div className='py-2 flex space-x-4 justify-end'>
-                                <div className='space-y-2'>
-                                    <h1 className='font-semibold text-right'>Agrobot</h1>
-                                    <p className='p-2 bg-gray-200 text-black rounded-l-xl rounded-b-xl text-xs'>
-                                        Farming is the practice of cultivating land, growing crops, and raising animals for food, fiber, medicinal plants, and other products used to sustain and enhance human life.
-                                        Crop Production: This includes planting, growing, and harvesting crops such as grains, fruits, vegetables, and legumes. Farmers use techniques like plowing, sowing, irrigating, and applying fertilizers and pesticides to ensure healthy and abundant crops.
-                                    </p>
-                                </div>
-                                <h1 className='h-10 w-10 flex items-center justify-center shrink-0 text-white  rounded-full bg-custom-green'>
-                                    <img src={chatbot} alt="" />
-                                </h1>                            </div>
+
+                        {/* looping through messages */}
+                        <div className='p-4 space-y-4 h-[65vh] overflow-auto'>
+                            {
+                                messages && messages.map((msg, index) =>
+                                    <div key={index} >
+                                        {msg.sender === "user" &&
+                                            <div className='py-2 flex space-x-4 justify-end'>
+                                                <div className='space-y-2'>
+                                                    <h1 className='font-semibold text-right'>You</h1>
+                                                    <div className='p-2 bg-blue-500 text-white rounded-l-xl rounded-b-xl text-xs'>
+                                                        <ReactMarkdown>
+                                                            {msg.text}
+                                                        </ReactMarkdown>
+                                                    </div>
+                                                </div>
+                                                <h1 className='h-10 w-10 flex items-center justify-center shrink-0 text-white  rounded-full bg-stone-900'>
+                                                    S
+                                                </h1>
+                                            </div>
+                                        }
+
+                                        {msg.sender === "bot" && <div className='py-2 flex space-x-4'>
+                                            <h1 className='h-10 w-10 flex items-center justify-center shrink-0 text-white  rounded-full bg-custom-green -800'>
+                                                <img src={chatbot} alt="" />
+
+                                            </h1>
+                                            <div className='space-y-2'>
+                                                <h1 className='font-semibold'>Agrobot</h1>
+                                                <div className='p-2 bg-gray-100 rounded-r-xl rounded-b-xl text-xs'>
+                                                    <ReactMarkdown>
+                                                        {msg.text}
+                                                    </ReactMarkdown>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        }
+                                    </div>
+                                )
+                            }
+                            {
+                                messages.length === 0 && <p className='text-center'>Start by asking the bot something</p>
+                            }
+                            {
+                                isLoading && <BeatLoader />
+                            }
                         </div>
+
                     </div>
                     <form onSubmit={generateAnswer} className='flex max-w-xl mx-auto items-center gap-1'>
                         <input
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,33 +1,12 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
-
-// https://vitejs.dev/config/
 export default defineConfig({
- plugins: [react()],
- resolve: {
-   alias: {
-     "@": path.resolve(__dirname, "./src"),
-   },
- },
-//  server: {
-//   proxy: {
-//    "/api": {
-//         target: "http://localhost:3000", // Ensure this server is running
-//         changeOrigin: true,
-//         secure: false, // `secure: false` if you're using an HTTP server
-//         rewrite: (path) => path.replace(/^\/api/, ""), // Rewrite the path for the proxy
-//       },
-//   },
-//  }
-}
-)
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
