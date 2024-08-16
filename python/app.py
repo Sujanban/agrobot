fe20f596f2705158fdb62a598aa4ce43875f3e39
Sujan Ban <bansujan@gmail.com>
2024-08-16 20:34:34 +0545

    workable disease detection implemented

--- python/app.py
+++ python/app.py
@@ -11,7 +11,7 @@ disease_info = pd.read_csv('disease_info.csv' , encoding='cp1252')
 supplement_info = pd.read_csv('supplement_info.csv',encoding='cp1252')
 
 model = CNN.CNN(39)    
-model.load_state_dict(torch.load("plant_disease_model_1_latest.pt"))
+model.load_state_dict(torch.load("model/plant_disease_model_1_latest.pt"))
 model.eval()
 
 def prediction(image_path):
